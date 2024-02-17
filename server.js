45d7d7ef8a239741ac2a600e2c1f7dcac78b9fec
Nisha Kumari <118532509+Nishak-061@users.noreply.github.com>
2024-02-17 19:51:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path';
 
 //configure env
 dotenv.config();
@@ -20,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, '../frontend/canteen-app/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -27,11 +29,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send({
-    message: "welcom",
-  });
-});
+app.use('*',function(req,res){  //new
+ res.sendFile(path.join(__dirname, '../frontend/canteen-app/build/index.html')) //new
+} )
 
 //PORT
 const PORT = process.env.PORT || 8080;
