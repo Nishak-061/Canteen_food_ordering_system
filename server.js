32bdd4fd7ac706dd65e80f9103533f9480674f99
Nisha Kumari <118532509+Nishak-061@users.noreply.github.com>
2024-02-17 20:04:31 +0530

    server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, '../frontend/canteen-app/build')))
+app.use(express.static(path.join(__dirname, './frontend/canteen-app/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -33,7 +33,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use('*',function(req,res){  //new
- res.sendFile(path.join(__dirname, '../frontend/canteen-app/build/index.html')) //new
+ res.sendFile(path.join(__dirname, './frontend/canteen-app/build/index.html')) //new
 } )
 
 //PORT
